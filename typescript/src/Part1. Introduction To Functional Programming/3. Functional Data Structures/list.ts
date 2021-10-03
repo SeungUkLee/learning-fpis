58072d8c05d6f5a617febc268efe74d259c5aa78
SeungukLee <seunguk@seunguk.dev>
2021-10-03 23:50:29 +0900

    edit

--- typescript/src/Part1. Introduction To Functional Programming/3. Functional Data Structures/list.ts
+++ typescript/src/Part1. Introduction To Functional Programming/3. Functional Data Structures/list.ts
@@ -9,6 +9,10 @@ class Cons<A> {
 
 export type List<A> = Cons<A> | Nil;
 
+export const consC = <A>(haed: A) => (tail: List<A>): List<A> => {
+  return new Cons(haed, tail);
+}
+
 export function cons<A>(head: A, tail: List<A>): List<A> {
   return new Cons(head, tail);
 }
@@ -164,7 +168,7 @@ export const numberToString = (l: List<number>): List<string> =>
   foldRight(l, nil() as List<string>)((n, acc) => cons(`${n}`, acc) )
 
 // Exercise 3-18
-export const map = <A, B>(al: List<A>) => (f: (a: A) => B): List<B> =>
+export const map = <A>(al: List<A>) => <B>(f: (a: A) => B): List<B> =>
   foldRight(al, nil() as List<B>)((a, acc) => cons(f(a), acc))
 
 // Exercise 3-19
@@ -172,7 +176,7 @@ export const filter = <A>(al: List<A>) => (f: (a: A) => boolean): List<A> =>
   foldRight(al, nil() as List<A>)((a, acc) => f(a) ? cons(a, acc) : acc)
 
 // Exercise 3-20
-export const flatMap = <A, B>(al: List<A>) => (f: (a: A) => List<B>): List<B> => {
+export const flatMap = <A>(al: List<A>) => <B>(f: (a: A) => List<B>): List<B> => {
   return flatten(map(al)(f) as List<List<B>>)
 }
 
@@ -197,5 +201,13 @@ export const zipWith = <A, B, C>(f: (a: A, b: B) => C, al: List<A>, bl: List<B>)
 
 // TODO: Exercise 3-24
 // export const hasSubsequence = <A>(sup: List<A>, sub: List<A>): boolean
+
+// ---
+export const fill = (n: number) => <A>(val: A): List<A> => {
+  const iter =  (n: number, val: A, acc: List<A>): List<A> => {
+    return n > 0 ? iter(n - 1, val, cons(val, acc)) : acc;
+  }
+  return iter(n, val, nil());
+}
 
 
