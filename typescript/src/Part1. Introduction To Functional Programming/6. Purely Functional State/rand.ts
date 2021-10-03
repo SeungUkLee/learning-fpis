897108db0a4844f968b627718d95a948c9765bba
SeungukLee <seunguk@seunguk.dev>
2021-10-03 23:52:44 +0900

    add: part1-6 exercises

---
+++ typescript/src/Part1. Introduction To Functional Programming/6. Purely Functional State/rand.ts
@@ -0,0 +1,180 @@
+import * as L from "../3. Functional Data Structures/list";
+
+type Int = number;
+type Double = number;
+
+interface RNG {
+  nextInt(): [Int, RNG]
+};
+
+class SimpleRNG implements RNG {
+  constructor(readonly seed: bigint) { }
+
+  nextInt(): [Int, RNG] {
+    const newSeed = (this.seed * 0x5DEECE66Dn + 0xBn) & 0xFFFFFFFFFFFFn;
+    const nextRNG = new SimpleRNG(newSeed);
+    const n = Number(newSeed >> 16n)
+
+    return [n, nextRNG];
+  }
+};
+
+
+// Exercises 6-1
+const nonNegativeInt = (rng: RNG): [Int, RNG] => {
+  const [n, r] = rng.nextInt();
+
+  return [ n > 0 ? n : - (n + 1), r ];
+};
+
+// Exercises 6-2
+const double = (rng: RNG): [Double, RNG] => {
+  const [n, r] = rng.nextInt();
+  const maxValue = Number.MAX_SAFE_INTEGER;
+
+  return [n / maxValue + 1, r];
+}
+
+// Exercises 6-3
+const intDouble = (rng: RNG): [[Int, Double], RNG] => {
+  const [n, r1] = rng.nextInt();
+  const [d, r2] = double(r1);
+
+  return [[n, d], r2];
+};
+
+const doubleInt = (rng: RNG): [[Double, Int], RNG] => {
+  const [[n, d], r] = intDouble(rng);
+  
+  return [[d, n], r];
+};
+
+const double3 = (rng: RNG): [[Double, Double, Double], RNG] => {
+  const [d1, r1] = double(rng);
+  const [d2, r2] = double(r1);
+  const [d3, r3] = double(r2);
+
+  return [[d1, d2, d3], r3];
+};
+
+// Exercises 6-4
+const ints = (count: Int) => (rng: RNG): [L.List<Int>, RNG] => {
+  const iter = (count: Int, r: RNG, acc: L.List<Int>): [L.List<Int>, RNG] => {
+    if (count === 0) {
+      return [acc, r];
+    } else {
+      const [n, r2] = r.nextInt();
+      return iter(count - 1, r2, L.cons(n, acc))
+    }
+  }
+
+  return iter(count, rng, L.nil());
+}
+
+// in 106p ~ 107p
+
+type Rand<A> = (rng: RNG) => [A, RNG];
+
+const int: Rand<Int> = (rng: RNG) => rng.nextInt();
+
+const unit = <A>(a: A): Rand<A> => {
+  return (rng) => [a, rng];
+};
+
+const map = <A>(s: Rand<A>) => <B>(f: (a: A) => B): Rand<B> => {
+  return (rng) => {
+    const [a, rng2] = s(rng);
+    return [f(a), rng2];
+  };
+};
+
+const nonNegativeEven : Rand<Int> = map(nonNegativeInt)((i) => i - i % 2);
+
+// Exercises 6-5
+const double2: Rand<Double> = map(nonNegativeInt)((n) => n / Number.MAX_SAFE_INTEGER + 1);
+
+// Exercises 6-6
+const map2 = <A, B>(ra: Rand<A>, rb: Rand<B>) => <C>(f: (a: A, b: B) => C): Rand<C> => {
+  return (rng) => {
+    const [a, r1] = ra(rng);
+    const [b, r2] = rb(r1);
+    return [f(a, b), r2];
+  }
+}
+
+// in 108p
+const both = <A, B>(ra: Rand<A>, rb: Rand<B>): Rand<[A, B]> =>
+  map2(ra, rb)((a, b) => [a, b]);
+
+const randIntDouble: Rand<[Int, Double]> = both(int, double);
+const randDoubleInt: Rand<[Double, Int]> = both(double, int);
+
+// Exercises 6-7
+// NOTE: Hard...
+const sequence = <A>(lr: L.List<Rand<A>>): Rand<L.List<A>> => {
+  const id = <A>(a: A) => a;
+  return traverse(lr)(id)
+}
+
+const apply = <A>(ra: Rand<A>) => <B>(rf: Rand<(a: A) => B>): Rand<B> => {
+  return (rng) => {
+    const [f, r1] = rf(rng)
+    const [a, r2] = ra(r1)
+    return [f(a), r2]
+  }
+}
+
+const traverse = <A>(l: L.List<A>) => <B>(f: (a: A) => Rand<B>): Rand<L.List<B>> => {
+  switch(l._tag) {
+    case 'Nil': {
+      return unit(L.nil());
+    }
+    case 'Cons': {
+      const { head, tail } = l;
+      return apply(traverse(tail)(f))(apply(f(head))(unit(L.consC)))
+    }
+  }
+}
+
+// use foldRight
+const traverse2 = <A>(l: L.List<A>) => <B>(f: (a: A) => Rand<B>): Rand<L.List<B>> =>
+  L.foldRight(l, unit(L.nil()) as Rand<L.List<B>>)((a, acc) => map2(f(a), acc)(L.cons))
+
+ 
+const ints2 = (count: Int): Rand<L.List<Int>>=>
+  sequence(L.fill(count)(int))
+
+
+// Exercises 6-8
+const flatMap = <A>(f: Rand<A>) => <B>(g: (a: A) => Rand<B>): Rand<B> => {
+  return (rng) => {
+    const [a, r] = f(rng);
+    return g(a)(r)
+  }
+}
+
+const nonNegativeLessThan = (n: Int): Rand<Int> =>
+  flatMap(nonNegativeInt)((i) => {
+    const mod = i % n;
+    return (i + (n - 1) - mod >= 0) ? unit(mod) : nonNegativeLessThan(n);
+  })
+
+
+// Exercises 6-9
+const map3 = <A>(s: Rand<A>) => <B>(f: (a: A) => B): Rand<B> =>
+  flatMap(s)((a) => unit(f(a)))
+
+
+const map4 = <A, B>(ra: Rand<A>, rb: Rand<B>) => <C>(f: (a: A, b: B) => C): Rand<C> =>
+  flatMap(ra)((a) => map(rb)((b) => f(a, b)))
+
+
+// in 111p ~ 112p
+const rollDie: Rand<Int> = nonNegativeLessThan(6);
+const sr = new SimpleRNG(5n);
+const zero = rollDie(sr)[0];
+const rollDie2: Rand<Int> = map(nonNegativeLessThan(6))((n) => n + 1)
+
+type State<S, A> = (s: S) => [A, S];
+type Rand2<A> = State<RNG, A>;
+
---
+++ typescript/src/Part1. Introduction To Functional Programming/6. Purely Functional State/state.ts
@@ -0,0 +1,48 @@
+import * as L from "../3. Functional Data Structures/list";
+
+// Exercises 6-10
+export type State<S, A> = (s: S) => [A, S];
+
+export const unit = <S, A>(a :A): State<S, A> => {
+  return (s) => [a, s];
+};
+
+export const map = <S, A>(st: State<S, A>) => <B>(f: (a: A) => B): State<S, B> =>
+  flatMap(st)((a) => unit(f(a)));
+
+export const map2 = <S, A, B>(st1: State<S, A>, st2: State<S, B>) => <C>(f: (a: A, b: B) => C): State<S, C> =>
+  flatMap(st1)((a) => map(st2)((b) => f(a, b)))
+
+export const flatMap = <S, A>(st: State<S, A>) => <B>(f: (a: A) => State<S, B>): State<S, B> => {
+  return (s) => {
+    const [a, s1] = st(s);
+    return f(a)(s1)
+  }
+};
+
+export const sequence = <S, A>(lst: L.List<State<S, A>>): State<S, L.List<A>> => 
+  traverse(lst)(id);
+
+const id = <A>(x: A): A => x;
+
+export const traverse = <A>(l: L.List<A>) => <S, B>(f: (a: A) => State<S, B>): State<S, L.List<B>> =>
+  L.foldRight(l, unit(L.nil()) as State<S, L.List<B>>)((a, acc) =>
+    map2(f(a), acc)(L.cons)
+  );
+
+// in 114p
+// TODO:
+// export const modify = <S>(f: (s: S) => S): State<S, void> => {
+// };
+
+export const get = <S>(): State<S, S> => {
+  return (s) => [s, s]
+};
+
+export const set = <S>(s: S): State<S, void> => {
+  return (_) => [undefined, s]
+}
+
+export const modify = <S>(f: (s: S) => S): State<S, void> =>
+  flatMap(get<S>())((s) => map(set(f(s)))((_) => undefined))
+
